73bfc3a32f11790461741474b94d25acaa951054
agnynureza <rezaagny@gmail.com>
2018-03-09 11:44:02 +0700

    delete on group

--- public/javascripts/Group.js
+++ public/javascripts/Group.js
@@ -21,14 +21,28 @@ $.ajax({
     success : function(respon){
         respon.forEach(data=>{
             $('#grouplist').prepend(`
-            <li class="collection-item avatar">
-            <i class="material-icons circle red">chat</i>
-            <span class="title">${data.title}</span>
-            <p>defaultf language : ${data.language}</p>
-            <a href="/messages/${data._id}" class="secondary-content"><i class="material-icons">input</i></a>
-          </li>
+            <li class="collection-item avatar group-${data._id}">
+                <i class="material-icons circle red">chat</i>
+                <span class="title">${data.title}</span>
+                <p>defaultf language : ${data.language}</p>
+                <div class = "secondary-content">
+                    <a id = "delete" onclick="deleteMessage('${data._id}')"><i class="material-icons">delete</i></a>
+                    <a href="/messages/${data._id}"><i class="material-icons">input</i></a>
+                </div>
+            </li>
             `)
         })
         }
     });
+
+function deleteMessage(id){
+    $.ajax({
+        url: 'http://localhost:3000/api/groups/delete/'+id,
+        type: 'DELETE',
+        success: function(result) {
+        $(`.group-${id}`).remove();
+        }
+    });
+}
+    
 
