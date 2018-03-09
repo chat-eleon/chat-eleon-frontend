2569c278077b4142462abdb73fc11f5c00823143
agrha <agrhakartz@gmail.com>
2018-03-09 16:12:30 +0700

    perbaikan footer

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/update', function(req, res, next) {
+  res.render('users/updateProfile')
 });
 
 module.exports = router;
